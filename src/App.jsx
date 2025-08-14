dd1425ded1300da2e3635e90df5a4e84de599149
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-14 09:33:27 +0800

    Set up technologies

---
+++ src/App.jsx
@@ -0,0 +1,16 @@
+import './App.css'
+import './index.css'
+
+function App() {
+
+  return (
+    <>
+      <div className="bg-blue-500 text-white p-8 text-center">
+        <h1 className="text-4xl font-bold mb-4">Marci Metzger</h1>
+        <p className="text-lg">This is a revamp of Marci Metzger Home Page</p>
+      </div>
+    </>
+  )
+}
+
+export default App
