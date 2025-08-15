06dc5b533a09c53efefd0662aa54f24cec915e26
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-15 16:20:03 +0800

    Make about section responsive

--- src/jsx/About.jsx
+++ src/jsx/About.jsx
@@ -1,61 +1,72 @@
 function About() {
   return (
-    <>
-      <div id="about" className="relative w-full min-h-screen">
-        {/* Background layers */}
-        <div className="absolute top-0 left-0 w-full h-[50%] bg-[#1e1e1e]" />
-        <div className="absolute bottom-0 left-0 w-full h-[50%] bg-transparent" />
+    <div
+  id="about"
+  className="
+    relative w-full min-h-screen flex items-center justify-center
+    pt-40 md:pt-0
+  "
+>
 
-        <div className="absolute w-[1000px] h-[400px] rounded-lg bg-[#303030] top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2">
-          {/* Content container inside the top part */}
-          <div className="absolute top-0 left-0 w-full h-[70%] flex items-center justify-center z-10">
-            <div className="flex flex-row items-start gap-10 max-w-[900px] px-4">
-              {/* Image + Name */}
-              <div className="flex flex-col items-center mt-[19%]">
-                <div
-                  className="
-                    w-[250px] h-[250px] rounded-full
-                    bg-[url('/marci.jpg')] bg-cover bg-center bg-no-repeat
-                  "
-                ></div>
-                <p className="text-white text-lg font-poppins font-light mt-4">
-                  Marci Metzger
-                </p>
-                <p className="text-white text-sm font-thin">
-                  Realtor
-                </p>
-              </div>
+      {/* Background layers */}
+      <div className="absolute top-0 left-0 w-full h-[50%] bg-[#1e1e1e]" />
+      <div className="absolute bottom-0 left-0 w-full h-[50%] bg-transparent" />
 
-              {/* Paragraphs + Logos */}
-              <div className="flex flex-col ml-10 mt-[18%] max-w-[500px]">
-                <p className="text-white text-[0.9rem] italic font-thin leading-5 mb-5">
-                  "I love that small-town feeling that our community offers. Spectacular golf
-                  courses, parks, pool, and easy access to Las Vegas make Pahrump a great
-                  place to call home. Working or retired, fast-paced or looking to relax...
-                  there's a place for you here! I enjoy living in the Mountain Falls
-                  community and will strive to find you a home that will suit you just as
-                  this community does me."
-                </p>
-                <p className="text-white text-[1rem] font-poppins font-thin leading-7 mb-6">
-                  Marci was a REALTOR, then licensed Broker, in Washington State. Now, she is
-                  enjoying the sunshine, and helping clients in Southern Nevada. Having helped
-                  buyers and sellers in many markets since 1995, she is a wealth of knowledge.
-                </p>
+      {/* Main content box */}
+      <div
+        className="
+          relative
+          w-[1000px] max-w-[95%] 
+          bg-[#303030] rounded-lg 
+          flex items-center justify-center
+          py-10
+        "
+        style={{ minHeight: "400px" }}
+      >
+        <div className="flex flex-col md:flex-row items-center md:items-start gap-6 md:gap-10 max-w-[900px] w-full">
+          {/* Image + Name */}
+          <div className="flex flex-col items-center">
+            <div
+              className="
+                w-[180px] h-[180px] md:w-[250px] md:h-[250px] 
+                rounded-full
+                bg-[url('/marci.jpg')] bg-cover bg-center bg-no-repeat
+              "
+            ></div>
+            <p className="text-white text-base md:text-lg font-poppins font-light mt-4">
+              Marci Metzger
+            </p>
+            <p className="text-white text-sm font-thin">Realtor</p>
+          </div>
 
-                {/* Logos row */}
-                <div className="flex justify-between items-center w-full mb-5">
-                  <div className="w-[60px] h-[60px] bg-[url('/logo-1.webp')] bg-cover bg-center bg-no-repeat"></div>
-                  <div className="w-[60px] h-[60px] bg-[url('/logo-2.webp')] bg-cover bg-center bg-no-repeat"></div>
-                  <div className="w-[60px] h-[60px] bg-[url('/logo-3.webp')] bg-cover bg-center bg-no-repeat"></div>
-                  <div className="w-[60px] h-[60px] bg-[url('/logo-4.webp')] bg-cover bg-center bg-no-repeat"></div>
-                </div>
-              </div>
+          {/* Text + Logos */}
+          <div className="flex flex-col md:ml-10 mt-6 md:mt-0 max-w-full md:max-w-[500px] text-center md:text-left">
+            <p className="text-white text-sm md:text-[0.9rem] italic font-thin leading-5 mb-5">
+              "I love that small-town feeling that our community offers. Spectacular golf
+              courses, parks, pool, and easy access to Las Vegas make Pahrump a great
+              place to call home. Working or retired, fast-paced or looking to relax...
+              there's a place for you here! I enjoy living in the Mountain Falls
+              community and will strive to find you a home that will suit you just as
+              this community does me."
+            </p>
+            <p className="text-white text-sm md:text-[1rem] font-poppins font-thin leading-6 md:leading-7 mb-6">
+              Marci was a REALTOR, then licensed Broker, in Washington State. Now, she is
+              enjoying the sunshine, and helping clients in Southern Nevada. Having helped
+              buyers and sellers in many markets since 1995, she is a wealth of knowledge.
+            </p>
+
+            {/* Logos */}
+            <div className="flex flex-wrap justify-center md:justify-between items-center w-full gap-4 mb-5">
+              <div className="w-[50px] h-[50px] md:w-[60px] md:h-[60px] bg-[url('/logo-1.webp')] bg-cover bg-center bg-no-repeat"></div>
+              <div className="w-[50px] h-[50px] md:w-[60px] md:h-[60px] bg-[url('/logo-2.webp')] bg-cover bg-center bg-no-repeat"></div>
+              <div className="w-[50px] h-[50px] md:w-[60px] md:h-[60px] bg-[url('/logo-3.webp')] bg-cover bg-center bg-no-repeat"></div>
+              <div className="w-[50px] h-[50px] md:w-[60px] md:h-[60px] bg-[url('/logo-4.webp')] bg-cover bg-center bg-no-repeat"></div>
             </div>
           </div>
         </div>
       </div>
-    </>
-  )
+    </div>
+  );
 }
 
-export default About
+export default About;
