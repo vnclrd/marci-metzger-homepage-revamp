3dbf8c8c2c3ab6c208d5a0ab43a8c884e9feacf6
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-15 19:04:15 +0800

    Make gallery section responsive

--- src/jsx/Gallery.jsx
+++ src/jsx/Gallery.jsx
@@ -1,32 +1,23 @@
 function Gallery() {
   return (
     <>
-      <div id="gallery" className="w-full min-h-screen bg-[#1e1e1e] flex items-center justify-center">
-        <div className="grid grid-cols-3 gap-6 mt-[5%]">
+      <div
+        id="gallery"
+        className="w-full min-h-screen bg-[#1e1e1e] flex justify-center px-4 py-10"
+      >
+        <div
+          className="
+            grid gap-6 mt-[5%]
+            grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
+          "
+        >
           {/* Photo items */}
-          <div
-            className="w-[400px] h-[250px] rounded-lg bg-[url('/img-1.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"
-          ></div>
-
-          <div
-            className="w-[400px] h-[250px] rounded-lg bg-[url('/img-2.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"
-          ></div>
-
-          <div
-            className="w-[400px] h-[250px] rounded-lg bg-[url('/img-3.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"
-          ></div>
-
-          <div
-            className="w-[400px] h-[250px] rounded-lg bg-[url('/img-4.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"
-          ></div>
-
-          <div
-            className="w-[400px] h-[250px] rounded-lg bg-[url('/img-5.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"
-          ></div>
-
-          <div
-            className="w-[400px] h-[250px] rounded-lg bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"
-          ></div>
+          <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-1.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
+          <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-2.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
+          <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-3.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
+          <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-4.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
+          <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-5.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
+          <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
         </div>
       </div>
     </>
