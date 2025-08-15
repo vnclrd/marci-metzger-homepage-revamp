ce00dedb8842f667a7313070a0b75f7260ff728b
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-15 12:28:26 +0800

    Add images to gallery

--- src/jsx/Gallery.jsx
+++ src/jsx/Gallery.jsx
@@ -1,9 +1,33 @@
 function Gallery() {
-
   return (
     <>
-      <div id='gallery' className='w-[100%] min-h-screen bg-[#1e1e1e]'>
+      <div id="gallery" className="w-full min-h-screen bg-[#1e1e1e] flex items-center justify-center">
+        <div className="grid grid-cols-3 gap-6 mt-[5%]">
+          {/* Photo items */}
+          <div
+            className="w-[300px] h-[250px] bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat"
+          ></div>
 
+          <div
+            className="w-[300px] h-[250px] bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat"
+          ></div>
+
+          <div
+            className="w-[300px] h-[250px] bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat"
+          ></div>
+
+          <div
+            className="w-[300px] h-[250px] bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat"
+          ></div>
+
+          <div
+            className="w-[300px] h-[250px] bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat"
+          ></div>
+
+          <div
+            className="w-[300px] h-[250px] bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat"
+          ></div>
+        </div>
       </div>
     </>
   )
