f375b01c51325112bd97a1dc157c7544b1c69e85
vnclrd <104509587+vnclrd@users.noreply.github.com>
2025-08-15 19:25:01 +0800

    Add slide in animation to gallery

--- src/jsx/Gallery.jsx
+++ src/jsx/Gallery.jsx
@@ -1,3 +1,5 @@
+import { motion } from "framer-motion";
+
 function Gallery() {
   return (
     <>
@@ -5,11 +7,15 @@ function Gallery() {
         id="gallery"
         className="w-full min-h-screen bg-[#1e1e1e] flex justify-center px-4 py-10"
       >
-        <div
+        <motion.div
           className="
             grid gap-6 mt-[5%]
             grid-cols-1 sm:grid-cols-2 lg:grid-cols-3
           "
+          initial={{ opacity: 0, y: 50 }}
+          whileInView={{ opacity: 1, y: 0 }}
+          transition={{ duration: 0.8 }}
+          viewport={{ once: true }}
         >
           {/* Photo items */}
           <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-1.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
@@ -18,10 +24,10 @@ function Gallery() {
           <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-4.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
           <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-5.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
           <div className="w-[400px] max-w-full h-[250px] rounded-lg bg-[url('/img-6.jpg')] bg-cover bg-center bg-no-repeat contrast-90 brightness-80"></div>
-        </div>
+        </motion.div>
       </div>
     </>
-  )
+  );
 }
 
-export default Gallery
+export default Gallery;
